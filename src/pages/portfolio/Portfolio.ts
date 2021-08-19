13ad82473cab0d1895c50a1449378f64190b63d6
Esteban <jelicich.e@gmail.com>
2021-08-19 19:02:03 +0200

    Add portfolio animations

--- src/pages/portfolio/Portfolio.ts
+++ src/pages/portfolio/Portfolio.ts
@@ -1,17 +1,20 @@
-import { Options, Vue } from "vue-class-component";
+import { Options, mixins } from "vue-class-component";
 import Modal from "@/components/modal/index.vue";
 import { PortfolioItem } from "@/models";
+import Animation from "@/mixins/Animation";
 
 @Options({
   components: { Modal },
 })
-export default class Portfolio extends Vue {
+export default class Portfolio extends mixins(Animation) {
   showModal = false;
   items: Array<PortfolioItem> = []; // = items;
   currentItem: PortfolioItem | null = null;
 
-  mounted(): void {
-    this.getProjects();
+  async mounted(): Promise<void> {
+    await this.getProjects();
+    this.animateBackground();
+    this.animateItems();
   }
 
   async getProjects(): Promise<void> {
@@ -30,5 +33,49 @@ export default class Portfolio extends Vue {
 
   closeModal(): void {
     this.showModal = false;
+  }
+
+  animateBackground(): void {
+    const TARGET_SEL = ".Portfolio-background";
+    const TRIGGER_ELEMENT_SEL = "#portfolio";
+
+    const animationOptions = {
+      runInMobile: true,
+      gsapOptions: {
+        scrollTrigger: {
+          trigger: TRIGGER_ELEMENT_SEL,
+          scrub: true,
+          start: "center bottom",
+          end: "bottom bottom",
+        },
+        x: this.helpers.vw(100),
+      },
+    };
+
+    this.timeline.from(TARGET_SEL, animationOptions);
+  }
+
+  animateItems(): void {
+    const TARGET_SEL = ".Portfolio-item";
+    const TRIGGER_ELEMENT_SEL = "#portfolio";
+
+    const items = document.querySelectorAll(TARGET_SEL);
+    items.forEach((item, i) => {
+      const offset = 1 + i / 5;
+      const animationOptions = {
+        runInMobile: true,
+        gsapOptions: {
+          scrollTrigger: {
+            trigger: TRIGGER_ELEMENT_SEL,
+            scrub: true,
+            start: `${this.helpers.vh(50) * offset} bottom`,
+            end: "bottom bottom",
+          },
+          y: this.helpers.vh(50),
+        },
+      };
+
+      this.timeline.from(item as HTMLElement, animationOptions);
+    });
   }
 }
